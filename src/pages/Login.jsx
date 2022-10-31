656b7324a1fc5972322724b70f71a7ce5260ceee
YoussefLMT <mcyoussef324@gmail.com>
2022-10-31 21:01:51 +0100

    add axios interceptors

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,10 +6,14 @@ function Login() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
+    const login = (e) => {
+       e.preventDefault()
+    }
+
     return (
         <section className="main">
             <div className="auth-content">
-                <form>
+                <form onSubmit={login}>
                     <h2 className="form-title">Login</h2>
                     <div>
                         <label>Email</label>
@@ -20,7 +24,7 @@ function Login() {
                         <input type="password" value={password} onChange={ (e) => setPassword(e.target.value)} name="password" className="text-input" />
                     </div>
                     <div>
-                        <button type="button" className="btn">Login</button>
+                        <button type="submit" className="btn">Login</button>
                     </div>
                 </form>
             </div >
