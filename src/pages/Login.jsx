219fa42227973a78e05d9bdd5a158126c7870fb0
YoussefLMT <mcyoussef324@gmail.com>
2022-10-31 18:57:17 +0100

    add login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Login() {
+    return (
+        <section class="main">
+            <div class="auth-content">
+                <form>
+                    <h2 class="form-title">Login</h2>
+                    <div>
+                        <label>Email</label>
+                        <input type="email" name="email" class="text-input" />
+                    </div>
+                    <div>
+                        <label>Password</label>
+                        <input type="password" name="password" class="text-input" />
+                    </div>
+                    <div>
+                        <button type="button" class="btn">Login</button>
+                    </div>
+                </form>
+            </div >
+        </section >
+    )
+}
+
+export default Login
