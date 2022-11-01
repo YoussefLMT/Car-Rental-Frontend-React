1dd4fd9ea50c2e28f393a05b89860523977facf0
YoussefLMT <mcyoussef324@gmail.com>
2022-11-01 12:46:51 +0100

    add sidebarData

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,20 @@
 import React from 'react'
+import { AiFillDashboard } from 'react-icons/ai';
+import { FiUsers } from 'react-icons/fi';
+
+const sidebarData = [
+    {
+        path: "/dashboard",
+        name: "dashboard",
+        icon: <AiFillDashboard />,
+    },
+
+    {
+        path: "/users",
+        name: "users",
+        icon: <FiUsers />,
+    },
+];
 
 function Sidebar() {
   return (
