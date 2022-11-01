d237997503648df0c8eadbeeb147b880c5c72f18
YoussefLMT <mcyoussef324@gmail.com>
2022-11-01 18:44:01 +0100

    add brands style

--- src/pages/Brands.jsx
+++ src/pages/Brands.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import Sidebar from '../components/Sidebar'
+import './styles/brands.css'
 function Brands() {
-  return (
-    <div>Brands</div>
-  )
+    return (
+        <>
+            <Sidebar />
+            <div className="content">
+                <div class="card brands" style={{ width: "1100px" }}>
+                    <div class="card-header">
+                        Brands Managment
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Brands
