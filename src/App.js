3117978ae01b1a8f6c11c79f92a2edd4169791eb
YoussefLMT <mcyoussef324@gmail.com>
2022-11-01 18:10:28 +0100

    protect admin routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import PrivateRoutes from './utils/PrivateRoutes';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={ localStorage.getItem('token') ? <Navigate to='/dashboard' /> : <Login />} />
-        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/' element={localStorage.getItem('token') ? <Navigate to='/dashboard' /> : <Login />} />
+
+        <Route element={<PrivateRoutes />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
