31c4bc79e9f45c5d4ce962eab2cb2eaf12f43596
YoussefLMT <mcyoussef324@gmail.com>
2022-11-01 18:53:43 +0100

    some extra changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axiosInstance from '../axios'
-import './login.css'
+import './styles/login.css'
 
 function Login() {
 
