020f439ba7866c390ec6ee3f2ae946ed67e88672
YoussefLMT <mcyoussef324@gmail.com>
2022-11-02 18:37:47 +0100

    add table

--- src/components/BrandsTable.jsx
+++ src/components/BrandsTable.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 
 function BrandsTable() {
-  return (
-    <div>BrandsTable</div>
-  )
+    return (
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">id</th>
+                    <th scope="col">name</th>
+                    <th scope="col">actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                        brands.map((brand) => {
+                            return (
+                                <tr key={brand.id}>
+                                    <th scope="row">{brand.id}</th>
+                                    <td>{brand.name}</td>
+                                </tr>
+                            )
+                        })
+                }
+            </tbody>
+        </table>
+    )
 }
 
 export default BrandsTable
--- src/pages/Brands.jsx
+++ src/pages/Brands.jsx
@@ -50,28 +50,7 @@ function Brands() {
                         </button>
                     </div>
                     <div class="card-body">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">id</th>
-                                    <th scope="col">name</th>
-                                    <th scope="col">actions</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    loading ? <h4>Loading...</h4> :
-                                        brands.map((brand) => {
-                                            return (
-                                                <tr key={brand.id}>
-                                                <th scope="row">{brand.id}</th>
-                                                <td>{brand.name}</td>
-                                                </tr>
-                                            )
-                                        })
-                                }
-                            </tbody>
-                        </table>
+                        
                     </div>
                 </div>
 
