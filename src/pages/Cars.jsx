e7c431d2b5e48fc157e8f558f028f69abb308859
YoussefLMT <mcyoussef324@gmail.com>
2022-11-02 20:18:50 +0100

    add car modal

--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -7,13 +7,68 @@ function Cars() {
         <>
             <Sidebar />
             <div className="content">
-             <h1>Cars</h1>
+                <h1>Cars</h1>
 
-             <div className="card brands" style={{ width: "1100px" }}>
+                <div className="card brands" style={{ width: "1100px" }}>
                     <div className="card-header">
                         Cars Management
-                    </div>   
-             </div>        
+
+                        {/* Button trigger add car modal  */}
+                        <button type="button" className="btn btn-primary btn-add" data-bs-toggle="modal" data-bs-target="#addCarModal">
+                            Add Car
+                        </button>
+
+
+
+                        {/* Add Car Modal  */}
+                        <div className="modal fade" id="addCarModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div className="modal-dialog">
+                                <div className="modal-content">
+                                    <div className="modal-header">
+                                        <h5 className="modal-title" id="exampleModalLabel">Add New Car</h5>
+                                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div className="modal-body">
+                                        <form>
+                                            <div className="mb-3">
+                                                <label htmlFor="name" className="form-label">Name</label>
+                                                <input type="text" className="form-control" id="name" />
+                                            </div>
+                                            <div className="mb-3">
+                                                <label htmlFor="model" className="form-label">Model Year</label>
+                                                <input type="text" className="form-control" id="model" />
+                                            </div>
+                                            <div className="mb-3">
+                                                <select class="form-select" aria-label="Default select example">
+                                                    <option selected>Brand</option>
+                                                    <option value="1">One</option>
+                                                    <option value="2">Two</option>
+                                                    <option value="3">Three</option>
+                                                </select>
+                                            </div>
+                                            <div className="mb-3">
+                                                <label htmlFor="color" className="form-label">Color</label>
+                                                <input type="text" className="form-control" id="color" />
+                                            </div>
+                                            <div className="mb-3">
+                                                <label htmlFor="capacity" className="form-label">Capacity</label>
+                                                <input type="text" className="form-control" id="capacity" />
+                                            </div>
+                                            <div className="mb-3">
+                                                <label htmlFor="plat" className="form-label">Plat Number</label>
+                                                <input type="text" className="form-control" id="plat" />
+                                            </div>
+                                            <div className="modal-footer">
+                                                <button type="submit" className="btn btn-primary">Save changes</button>
+                                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </>
