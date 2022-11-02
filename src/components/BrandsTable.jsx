2278a3a6a8229d333f8f684c8e23f25766558432
YoussefLMT <mcyoussef324@gmail.com>
2022-11-02 18:53:58 +0100

    add deleteBrand function

--- src/components/BrandsTable.jsx
+++ src/components/BrandsTable.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import axiosInstance from '../axios'
 
 function BrandsTable(props) {
+
+    const deleteBrand = async (e, id) => {
+        const deleteBtn = e.currentTarget;
+
+        try {
+            await axiosInstance.delete(`/delete-brand/${id}`)
+            deleteBtn.closest('tr').remove();
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
     return (
-        <table class="table">
+        <table className="table">
             <thead>
                 <tr>
                     <th scope="col">id</th>
@@ -12,17 +27,20 @@ function BrandsTable(props) {
             </thead>
             <tbody>
                 {
-                        props.brands.map((brand) => {
-                            return (
-                                <tr key={brand.id}>
-                                    <th scope="row">{brand.id}</th>
-                                    <td>{brand.name}</td>
-                                </tr>
-                            )
-                        })
+                    props.brands.map((brand) => {
+                        return (
+                            <tr key={brand.id}>
+                                <th scope="row">{brand.id}</th>
+                                <td>{brand.name}</td>
+                                <td>
+                                    <button type="button" onClick={(e) => deleteBrand(e, brand.id)} class="btn btn-danger">Delete</button>
+                                </td>
+                            </tr>
+                        )
+                    })
                 }
             </tbody>
-        </table>
+        </table >
     )
 }
 
