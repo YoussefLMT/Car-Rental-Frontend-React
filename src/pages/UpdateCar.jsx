565de872646eaa22c4689ac487945d3c01c8d6d6
YoussefLMT <mcyoussef324@gmail.com>
2022-11-03 12:31:45 +0100

    add getBrands function

--- src/pages/UpdateCar.jsx
+++ src/pages/UpdateCar.jsx
@@ -7,19 +7,26 @@ import './styles/cars.css'
 function UpdateCar() {
 
   const [car, setCar] = useState({})
+  const [brands, setBrands] = useState([])
   const params = useParams()
 
   const getCar = async () => {
     const response = await axiosInstance.get(`get-car/${params.id}`)
-    if(response.data.status === 200){
+    if (response.data.status === 200) {
       setCar(response.data.car)
-    }else if(response.data.status === 404){
+    } else if (response.data.status === 404) {
       console.log(response.data.message)
     }
   }
 
+  const getBrands = async () => {
+    const response = await axiosInstance.get('brands')
+    setBrands(response.data.brands)
+  }
+
   useEffect(() => {
     getCar()
+    getBrands()
   }, [])
 
   return (
@@ -35,28 +42,35 @@ function UpdateCar() {
             <form>
               <div className="mb-3">
                 <label htmlFor="name" className="form-label">Name</label>
-                <input type="text" name='name' className="form-control" id="name" />
+                <input type="text" value={car.name} name='name' className="form-control" id="name" />
               </div>
               <div className="mb-3">
                 <label htmlFor="model" className="form-label">Model Year</label>
-                <input type="text" name='model_year' className="form-control" id="model" />
+                <input type="text" value={car.model_year} name='model_year' className="form-control" id="model" />
               </div>
               <div className="mb-3">
-                <select class="form-select" name='brand' aria-label="Default select example">
+                <select class="form-select" value={car.brand} name='brand' aria-label="Default select example">
                   <option selected>Brand</option>
+                  {
+                    brands.map((brand) => {
+                      return (
+                        <option value={brand.name} key={brand.id}>{brand.name}</option>
+                      )
+                    })
+                  }
                 </select>
               </div>
               <div className="mb-3">
                 <label htmlFor="color" className="form-label">Color</label>
-                <input type="text" name='color' className="form-control" id="color" />
+                <input type="text" value={car.color} name='color' className="form-control" id="color" />
               </div>
               <div className="mb-3">
                 <label htmlFor="capacity" className="form-label">Capacity</label>
-                <input type="text" name='capacity' className="form-control" id="capacity" />
+                <input type="text" value={car.capacity} name='capacity' className="form-control" id="capacity" />
               </div>
               <div className="mb-3">
                 <label htmlFor="plate" className="form-label">Plate Number</label>
-                <input type="text" name='plate_number' className="form-control" id="plate" />
+                <input type="text" value={car.plate_number} name='plate_number' className="form-control" id="plate" />
               </div>
               <button type="submit" className="btn btn-primary">Save changes</button>
             </form>
