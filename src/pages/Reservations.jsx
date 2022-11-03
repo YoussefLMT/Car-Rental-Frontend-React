61723bb0ab0723d7fed2013fd483907c8e8a1da7
YoussefLMT <mcyoussef324@gmail.com>
2022-11-03 18:30:23 +0100

    add sidebar and content

--- src/pages/Reservations.jsx
+++ src/pages/Reservations.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Sidebar from '../components/Sidebar'
 
 function Reservations() {
-  return (
-    <div>Reservations</div>
-  )
+    return (
+        <>
+            <Sidebar />
+            <div className="content">
+                <h1>Reservations</h1>
+
+                <div className="card brands" style={{ width: "1100px" }}>
+                    <div className="card-header">
+                       Reservations Management
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Reservations
