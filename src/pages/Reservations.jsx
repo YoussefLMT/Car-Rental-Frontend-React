08695eb0a9fae991c5f98bb6d3f0fe8d79f4a5b3
YoussefLMT <mcyoussef324@gmail.com>
2022-11-03 18:31:14 +0100

    add style

--- src/pages/Reservations.jsx
+++ src/pages/Reservations.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar'
+import './styles/reservations.css'
 
 function Reservations() {
     return (
@@ -8,7 +9,7 @@ function Reservations() {
             <div className="content">
                 <h1>Reservations</h1>
 
-                <div className="card brands" style={{ width: "1100px" }}>
+                <div className="card resrvations" style={{ width: "1100px" }}>
                     <div className="card-header">
                        Reservations Management
                     </div>
