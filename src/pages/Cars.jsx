660d8dc827ce61dba32ea32cb10d68ec5e0fdd91
YoussefLMT <mcyoussef324@gmail.com>
2022-11-03 18:42:03 +0100

    add modal

--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -78,7 +78,7 @@ function Cars() {
                         <button type="button" className="btn btn-primary btn-add" data-bs-toggle="modal" data-bs-target="#addCarModal">
                             Add Car
                         </button>
-                        </div>
+                    </div>
                     <div class="card-body">
                         <CarsTable cars={cars} />
 
@@ -86,70 +86,69 @@ function Cars() {
                     </div>
                 </div>
 
-                        {/* Add Car Modal  */}
-                        <div className="modal fade" id="addCarModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div className="modal-dialog">
-                                <div className="modal-content">
-                                    <div className="modal-header">
-                                        <h5 className="modal-title" id="exampleModalLabel">Add New Car</h5>
-                                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                {/* Add Car Modal  */}
+                <div className="modal fade" id="addCarModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalLabel">Add New Car</h5>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div className="modal-body">
+                                <form onSubmit={addNewCar}>
+                                    {
+                                        message && <div className="alert alert-success">
+                                            {message}
+                                        </div>
+                                    }
+                                    <div className="mb-3">
+                                        <label htmlFor="name" className="form-label">Name</label>
+                                        <input type="text" name='name' value={form.name} onChange={handleChange} className="form-control" id="name" />
+                                        <span className='text-danger'>{form.error_list.name}</span>
                                     </div>
-                                    <div className="modal-body">
-                                        <form onSubmit={addNewCar}>
+                                    <div className="mb-3">
+                                        <label htmlFor="model" className="form-label">Model Year</label>
+                                        <input type="text" name='model_year' value={form.model_year} onChange={handleChange} className="form-control" id="model" />
+                                        <span className='text-danger'>{form.error_list.model_year}</span>
+                                    </div>
+                                    <div className="mb-3">
+                                        <select class="form-select" name='brand' value={form.brand} onChange={handleChange} aria-label="Default select example">
+                                            <option selected>Brand</option>
                                             {
-                                                message && <div className="alert alert-success">
-                                                    {message}
-                                                </div>
+                                                brands.map((brand) => {
+                                                    return (
+                                                        <option value={brand.name} key={brand.id}>{brand.name}</option>
+                                                    )
+                                                })
                                             }
-                                            <div className="mb-3">
-                                                <label htmlFor="name" className="form-label">Name</label>
-                                                <input type="text" name='name' value={form.name} onChange={handleChange} className="form-control" id="name" />
-                                                <span className='text-danger'>{form.error_list.name}</span>
-                                            </div>
-                                            <div className="mb-3">
-                                                <label htmlFor="model" className="form-label">Model Year</label>
-                                                <input type="text" name='model_year' value={form.model_year} onChange={handleChange} className="form-control" id="model" />
-                                                <span className='text-danger'>{form.error_list.model_year}</span>
-                                            </div>
-                                            <div className="mb-3">
-                                                <select class="form-select" name='brand' value={form.brand} onChange={handleChange} aria-label="Default select example">
-                                                    <option selected>Brand</option>
-                                                    {
-                                                        brands.map((brand) => {
-                                                            return (
-                                                                <option value={brand.name} key={brand.id}>{brand.name}</option>
-                                                            )
-                                                        })
-                                                    }
-                                                </select>
-                                                <span className='text-danger'>{form.error_list.brand}</span>
-                                            </div>
-                                            <div className="mb-3">
-                                                <label htmlFor="color" className="form-label">Color</label>
-                                                <input type="text" name='color' value={form.color} onChange={handleChange} className="form-control" id="color" />
-                                                <span className='text-danger'>{form.error_list.color}</span>
-                                            </div>
-                                            <div className="mb-3">
-                                                <label htmlFor="capacity" className="form-label">Capacity</label>
-                                                <input type="text" name='capacity' value={form.capacity} onChange={handleChange} className="form-control" id="capacity" />
-                                                <span className='text-danger'>{form.error_list.capacity}</span>
-                                            </div>
-                                            <div className="mb-3">
-                                                <label htmlFor="plate" className="form-label">Plate Number</label>
-                                                <input type="text" name='plate_number' value={form.plate_number} onChange={handleChange} className="form-control" id="plate" />
-                                                <span className='text-danger'>{form.error_list.plate_number}</span>
-                                            </div>
-                                            <div className="modal-footer">
-                                                <button type="submit" className="btn btn-primary">Save changes</button>
-                                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                            </div>
-                                        </form>
+                                        </select>
+                                        <span className='text-danger'>{form.error_list.brand}</span>
                                     </div>
-                                </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="color" className="form-label">Color</label>
+                                        <input type="text" name='color' value={form.color} onChange={handleChange} className="form-control" id="color" />
+                                        <span className='text-danger'>{form.error_list.color}</span>
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="capacity" className="form-label">Capacity</label>
+                                        <input type="text" name='capacity' value={form.capacity} onChange={handleChange} className="form-control" id="capacity" />
+                                        <span className='text-danger'>{form.error_list.capacity}</span>
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="plate" className="form-label">Plate Number</label>
+                                        <input type="text" name='plate_number' value={form.plate_number} onChange={handleChange} className="form-control" id="plate" />
+                                        <span className='text-danger'>{form.error_list.plate_number}</span>
+                                    </div>
+                                    <div className="modal-footer">
+                                        <button type="submit" className="btn btn-primary">Save changes</button>
+                                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    </div>
+                                </form>
                             </div>
                         </div>
+                    </div>
+                </div>
             </div>
-
         </>
     )
 }
--- src/pages/Reservations.jsx
+++ src/pages/Reservations.jsx
@@ -12,6 +12,60 @@ function Reservations() {
                 <div className="card resrvations" style={{ width: "1100px" }}>
                     <div className="card-header">
                        Reservations Management
+
+                       {/* Button trigger add car reservation  */}
+                       <button type="button" className="btn btn-primary btn-add" data-bs-toggle="modal" data-bs-target="#addReservationModal">
+                            Add Reservation
+                        </button>
+                    </div>
+                </div>
+
+                 {/* Add Reservation Modal  */}
+                 <div className="modal fade" id="addReservationModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalLabel">Add New Car</h5>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div className="modal-body">
+                                <form>
+                                    <div className="mb-3">
+                                        <label htmlFor="fname" className="form-label">First Name</label>
+                                        <input type="text" name='first_name' className="form-control" id="fname" />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="lname" className="form-label">Last Name</label>
+                                        <input type="text" name='last_name' className="form-control" id="lname" />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="phone" className="form-label">Phone</label>
+                                        <input type="text" name='phonr' className="form-control" id="phone" />
+                                    </div>
+                                    <div className="mb-3">
+                                        <select class="form-select" name='car_id' aria-label="Default select example">
+                                            <option selected>Car</option>
+                                        </select>
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="start_date" className="form-label">Start Date</label>
+                                        <input type="date" name='start_date' className="form-control" id="start_date" />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="end_date" className="form-label">End Date</label>
+                                        <input type="date" name='end_date' className="form-control" id="end_date" />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="amount" className="form-label">Total amount</label>
+                                        <input type="text" name='total_amount' className="form-control" id="amount" />
+                                    </div>
+                                    <div className="modal-footer">
+                                        <button type="submit" className="btn btn-primary">Save changes</button>
+                                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
