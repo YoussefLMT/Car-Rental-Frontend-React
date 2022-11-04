bffe1c292b68acbc9a8afb32f1ab2219514d95c3
YoussefLMT <mcyoussef324@gmail.com>
2022-11-04 20:19:35 +0100

    add card to dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axiosInstance from '../axios'
+import Card from '../components/Card'
 import Sidebar from '../components/Sidebar'
 
 function Dashboard() {
@@ -27,6 +28,20 @@ function Dashboard() {
       <Sidebar />
       <div className="content">
         <h1>Dashboard</h1>
+        <div class="row">
+          <div class="col-md-3">
+            <Card icon="fa-solid fa-burger" title="Meals" count={carsCount} />
+          </div>
+          <div class="col-md-3">
+            <Card icon="fa-brands fa-shopify" title="Orders" count={reservationsCount} />
+          </div>
+          <div class="col-md-3">
+            <Card icon="fa-solid fa-users" title="Users" count={usersCount} />
+          </div>
+          <div class="col-md-3">
+            <Card icon="fa-solid fa-dollar-sign" title="Income" count={income} />
+          </div>
+        </div>
       </div>
     </>
   )
