81f47c9889f6660790cf37ffc60db9ead94cfd0b
YoussefLMT <mcyoussef324@gmail.com>
2022-11-04 18:41:27 +0100

    add table content

--- src/components/ReservationsTable.jsx
+++ src/components/ReservationsTable.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 
 function ReservationsTable() {
-  return (
-    <div>ReservationsTable</div>
-  )
+    return (
+        <table className="table">
+            <thead>
+                <tr>
+                    <th scope="col">id</th>
+                    <th scope="col">name</th>
+                    <th scope="col">model year</th>
+                    <th scope="col">brand</th>
+                    <th scope="col">color</th>
+                    <th scope="col">capacity</th>
+                    <th scope="col">plate number</th>
+                    <th scope="col">actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    props.cars.map((car) => {
+                        return (
+                            <tr key={car.id}>
+                                <th scope="row">{car.id}</th>
+                                <td>{car.name}</td>
+                                <td>{car.model_year}</td>
+                                <td>{car.brand}</td>
+                                <td>{car.color}</td>
+                                <td>{car.capacity}</td>
+                                <td>{car.plate_number}</td>
+                            </tr>
+                        )
+                    })
+                }
+            </tbody>
+        </table >
+    )
 }
 
 export default ReservationsTable
