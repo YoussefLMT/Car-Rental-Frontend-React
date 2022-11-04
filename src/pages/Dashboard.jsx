f4ea371f8ad1b2903fd0e2340d5e0aa61df8ee7c
YoussefLMT <mcyoussef324@gmail.com>
2022-11-04 20:17:07 +0100

    add getTotalCount function

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axiosInstance from '../axios'
 import Sidebar from '../components/Sidebar'
 
 function Dashboard() {
+
+  const [carsCount, setCarsCount] = useState({})
+  const [usersCount, setUsersCount] = useState({})
+  const [reservationsCount, setReservationsCount] = useState({})
+  const [income, setIncome] = useState({})
+
+
+  const getTotalCount = async () => {
+    const response = await axiosInstance.get('/statistics')
+    setCarsCount(response.data.carsCount)
+    setUsersCount(response.data.usersCount)
+    setReservationsCount(response.data.reservationsCount)
+    setIncome(response.data.income)
+  }
+
+  useEffect(() => {
+    getTotalCount()
+  }, [])
+
   return (
     <>
       <Sidebar />
       <div className="content">
-      <h1>Dashboard</h1>
+        <h1>Dashboard</h1>
       </div>
     </>
   )
