1f3c32afc3a3b8f968a7fb894242cf1d16886e3f
YoussefLMT <mcyoussef324@gmail.com>
2022-11-04 20:06:35 +0100

    add card content

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
-function Card() {
-  return (
-    <div>Card</div>
-  )
+function Card(props) {
+    return (
+        <div id="root">
+            <div class="container mt-4">
+                <div class="c-dashboardInfo">
+                    <div class="wrap">
+                        <h4 class="c-dashboardInfo__title"><i class={props.icon}></i>{ props.title }</h4><span class="c-dashboardInfo__count">{ props.count }</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Card
