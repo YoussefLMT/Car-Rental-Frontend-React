4e9ecd6813269edbf7878112dd52cfee35dc0ab3
YoussefLMT <mcyoussef324@gmail.com>
2022-11-04 19:05:48 +0100

    add getReservation function

--- src/pages/UpdateReservation.jsx
+++ src/pages/UpdateReservation.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
 import Sidebar from '../components/Sidebar'
 import './styles/reservations.css'
 
 function UpdateReservation() {
+
+    const [cars, setCars] = useState({})
+    const [reservation, setReservation] = useState([])
+    const [errors, setErrors] = useState([]);
+    const params = useParams()
+
+    const getReservation = async () => {
+        const response = await axiosInstance.get(`get-reservation/${params.id}`)
+        if (response.data.status === 200) {
+            setReservation(response.data.reservation)
+        } else if (response.data.status === 404) {
+            console.log(response.data.message)
+        }
+    }
+
+    useEffect(() => {
+        getReservation()
+    }, [])
+
     return (
         <>
             <Sidebar />
