c16f3d1a1b2783bd90d252ce6d46ec20a4bf074c
YoussefLMT <mcyoussef324@gmail.com>
2022-11-08 19:39:22 +0100

    add active link

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { AiFillDashboard, AiFillCar } from 'react-icons/ai';
 import { FiUsers } from 'react-icons/fi';
 import { FaCarSide, FaCalendarAlt } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './sidebar.css';
 
 const sidebarData = [
@@ -39,7 +39,7 @@ function Sidebar() {
             <div className="sidebar">
                 <section className="routes">
                     {sidebarData.map((data) => (
-                        <Link to={data.path} key={data.name} className="link">
+                        <NavLink to={data.path} key={data.name} className="link">
                             <div className="icon">
                                 {data.icon}
                             </div>
@@ -47,7 +47,7 @@ function Sidebar() {
                             <div className="text">
                                 {data.name}
                             </div>
-                        </Link>
+                        </NavLink>
                     ))}
                 </section>
             </div>
